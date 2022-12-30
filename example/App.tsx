323eaa0d3fe3090c0375fe9c012801d63ffc7530
Mike Bybee <me@mikebybee.com>
2022-12-30 11:46:53 -0600

    build[npm]: ⬆️ update dependencies

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,12 @@
+import { Suspense } from "react";
+import { createBrowserRouter, RouterProvider } from "@onerouter/core";
+import useSWR from "swr";
 import "./App.css";
 import "antd/dist/reset.css";
 import NotAuth from "./pages/403";
 import Loading from "./components/Loading";
 import { getAuthRouters } from "../lib";
-import useSWR from "swr";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routers } from "./routers";
-import { Suspense } from "react";
 
 const fetcher = async (url: string): Promise<string[]> =>
   await new Promise((resolve) => {
